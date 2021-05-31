d3f8ba7fe263ede83dcd6937eaa6580ad8986db3
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-05-31 08:40:17 +0300

    Add a blacklist using string interning

--- src/profiler.rs
+++ src/profiler.rs
@@ -1,4 +1,5 @@
-use splay::SplayMap;
+use splay::{SplayMap, SplaySet};
+use string_interner::{symbol::SymbolU32, StringInterner};
 
 use crate::{
     stopwatch::{Statistics, Stopwatch},
@@ -10,14 +11,18 @@ use crate::{
 ///
 /// Should be kept in a thread-local variable.
 pub struct Profiler {
+    interner: StringInterner,
+    blacklist: SplaySet<SymbolU32>,
     stack: Vec<Stopwatch<TimeCounter>>,
-    times: SplayMap<String, Vec<Statistics<TimeCounter>>>,
+    times: SplayMap<SymbolU32, Vec<Statistics<TimeCounter>>>,
 }
 
 impl Profiler {
     /// Initializes a new profiler state.
     pub fn new() -> Self {
         Self {
+            interner: StringInterner::new(),
+            blacklist: SplaySet::new(),
             stack: Vec::with_capacity(1024),
             times: SplayMap::new(),
         }
@@ -25,8 +30,7 @@ impl Profiler {
 
     /// Resets the profiler's internal data structures.
     pub fn reset(&mut self) {
-        self.stack = Vec::with_capacity(1024);
-        self.times = SplayMap::new();
+        *self = Self::new();
     }
 
     /// Called when a function is called.
@@ -34,10 +38,17 @@ impl Profiler {
     /// # Arguments
     ///
     /// * `name` - Name of the called function.
-    pub fn on_call(&mut self, _name: &str) {
+    pub fn on_call(&mut self, name: &str) {
+        let sym = self.interner.get_or_intern(name);
+
+        if self.blacklist.contains(&sym) {
+            return;
+        }
+
         if let Some(stopwatch) = self.stack.last_mut() {
             stopwatch.pause();
         }
+
         self.stack.push(Stopwatch::new(TimeCounter));
         self.stack.last_mut().unwrap().start();
     }
@@ -48,17 +59,23 @@ impl Profiler {
     ///
     /// * `name` - Name of the returning function.
     pub fn on_return(&mut self, name: &str) {
+        let sym = self.interner.get_or_intern(name);
+
+        if self.blacklist.contains(&sym) {
+            return;
+        }
+
         // If we're not returning from the top-most function
         if let Some(mut stopwatch) = self.stack.pop() {
             // Stop the associated stopwatch
             let duration = stopwatch.stop();
 
             // Save the execution time
-            if !self.times.contains_key(name) {
-                self.times.insert(name.to_string(), Vec::new());
+            if !self.times.contains_key(&sym) {
+                self.times.insert(sym, Vec::new());
             }
 
-            let times = self.times.get_mut(name).unwrap();
+            let times = self.times.get_mut(&sym).unwrap();
             times.push(duration);
         }
 
@@ -66,6 +83,8 @@ impl Profiler {
         if let Some(stopwatch) = self.stack.last_mut() {
             stopwatch.unpause();
         }
+
+        self.blacklist.insert(sym);
     }
 
     /// Returns a vector of the profiling statistics gathered so far.
@@ -73,7 +92,8 @@ impl Profiler {
         self.times
             .clone()
             .into_iter()
-            .map(|(name, times)| {
+            .map(|(sym, times)| {
+                let name = self.interner.resolve(sym).unwrap().to_owned();
                 let total_time = times.iter().map(|d| d.total.as_nanos()).sum();
                 let cumulative_time: u128 = times.iter().map(|d| d.cumulative.as_nanos()).sum();
                 let num_calls = times.len();
